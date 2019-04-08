e9139d7c538157933592bd7b55bc881eeadb11e5
jiangjian0206 <756028992@qq.com>
2019-04-08 18:54:41 +0800

    9day v2

--- src/router.js
+++ src/router.js
@@ -7,9 +7,13 @@ import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
 import NewsList from './components/news/NewsList.vue'
 import NewsInfo from './components/news/NewsInfo.vue'
+import PhotoList from './components/photos/PhotoList.vue'
+import PhotoInfo from './components/photos/PhotoInfo.vue'
+// import GoodsList from './components/goods/GoodsList.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
+  // mode: 'history',
   routes: [ // 配置路由规则
     { path: '/', redirect: '/home' },
     { path: '/home', component: HomeContainer },
@@ -17,7 +21,9 @@ var router = new VueRouter({
     { path: '/shopcar', component: ShopcarContainer },
     { path: '/search', component: SearchContainer },
     { path: '/home/newslist', component: NewsList },
-    { path: '/home/newsinfo/:id', component: NewsInfo }
+    { path: '/home/newsinfo/:id', component: NewsInfo },
+    { path: '/home/photolist', component: PhotoList },
+    { path: '/home/photoinfo/:id', component: PhotoInfo },
   ],
   linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 })
